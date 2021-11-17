aaab5fe391f368ef1e25c0223b578b56ce1aec82
Administrator <chouchane.marwen@gmail.com>
2021-11-17 12:26:42 +0000

    6eme commit : Commit new package 'Methode' / classe  'First Method'

--- src/methode/firstMethod.java
+++ src/methode/firstMethod.java
@@ -1,4 +1,18 @@
 package methode;
 
+import java.util.Scanner;
+
+/*
+Methode to ask user for their name than greet them by name.
+ */
 public class firstMethod {
+    public static void main(String arg[]) {
+    System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        greetUser(name);
+    }
+    public static void greetUser (String name){
+        System.out.println("Hi there "+name);
+    }
 }

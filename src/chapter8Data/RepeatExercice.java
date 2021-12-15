76c3d7d09d1fff59fd4e0fd0536adfa425b3d775
Administrator <chouchane.marwen@gmail.com>
2021-12-15 15:03:59 +0000

    ExcerciceChangePasswordValidator

--- src/chapter8Data/RepeatExercice.java
+++ src/chapter8Data/RepeatExercice.java
@@ -2,16 +2,49 @@ package chapter8Data;
 
 import java.util.Scanner;
 
-public class ExcerciePassewordValidator {
+public class RepeatExercice {
     private static Scanner scanner = new Scanner(System.in);
-    private String username;
-    private String currentPassword;
-    private boolean valid;
-    private String errorMessage;
+    private String userName;
+    private String passeWord;
+    private boolean valid ;
+    private String errorMessage ;
 
-    public ExcerciePassewordValidator(String username, String currentPassword) {
-        this.username = username;
-        this.currentPassword = currentPassword;
+    private  RepeatExercice(String userName, String passeWord){
+        this.userName = userName;
+        this.passeWord = passeWord;
+    }
+    public static void main (String[] args){
+        var validator = login();
+        validator.printErrorsMessages();
+        do{
+            var newPassword = validator.getProposedPassword();
+            validator.getConditionChangeLogin(newPassword);
+            if(!validator.isValid()){
+                System.out.println(validator.getErrorMessage());
+            }
+        }while (!validator.isValid());
+        System.out.println("The proposed password is valid");
+        validator.closeScanner();
+    }
+    public String getProposedPassword(){
+        System.out.println("Enter your new password:");
+        return scanner.nextLine();
+    }
+    private void printErrorsMessages(){
+        System.out.println("Your new password must meet the following requirements:");
+        System.out.println("* at least 8 characters long");
+        System.out.println("* contain an uppercase letter");
+        System.out.println("* contain a special character");
+        System.out.println("* not contain the username");
+        System.out.println("* not the same as the old password");
+        System.out.println();
+    }
+    private static RepeatExercice login(){
+        System.out.println("Enter your username:");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+        return new RepeatExercice(username ,password);
     }
     public boolean isValid(){
         return valid;
@@ -22,48 +55,12 @@ public class ExcerciePassewordValidator {
     public void closeScanner(){
         scanner.close();
     }
-    public static void main (String[] args){
-       var validator = login();
-       validator.printPasswordRules();
-        do{
-            var proposedPassword = validator.getProposedPassword();
-            validator.changePassword(proposedPassword);
-            if(!validator.isValid()){
-                System.out.println(validator.getErrorMessage());
-            }
-        }while(!validator.isValid());
-        System.out.println("The proposed password is valid");
-        validator.closeScanner();
-    }
-
-    public static ExcerciePassewordValidator login(){
-        System.out.println("Enter your username:");
-        String username = scanner.nextLine();
-
-        System.out.println("Enter your password:");
-        String password = scanner.nextLine();
-
-        return new ExcerciePassewordValidator(username, password);
-    }
-    public void printPasswordRules(){
-        System.out.println("Your new password must meet the following requirements:");
-        System.out.println("* at least 8 characters long");
-        System.out.println("* contain an uppercase letter");
-        System.out.println("* contain a special character");
-        System.out.println("* not contain the username");
-        System.out.println("* not the same as the old password");
-        System.out.println();
-    }
-    public String getProposedPassword(){
-        System.out.println("Enter your new password:");
-        return scanner.nextLine();
-    }
-    public void changePassword(String newPassword) {
+    private void getConditionChangeLogin(String newPassword){
         valid = true;
         errorMessage = "";
         if (newPassword.length() < 8) {
             valid = false;
-            errorMessage += "\n Your password must be at least 8 characters.";
+            errorMessage += " Your password must be at least 8 characters.";
         }
         if(newPassword.equals(newPassword.toLowerCase())){
             valid = false;
@@ -75,12 +72,12 @@ public class ExcerciePassewordValidator {
             errorMessage += "\n Your password must include a special character (e.g. %,$[:).";
         }
 
-        if(newPassword.toUpperCase().contains(username.toUpperCase())){
+        if(newPassword.toUpperCase().contains(userName.toUpperCase())){
             valid = false;
             errorMessage += "\n Your password cannot contain your username.";
         }
 
-        if(newPassword.equals(currentPassword)){
+        if(newPassword.equals(passeWord)){
             valid = false;
             errorMessage += "\n Your password must be different from your current password.";
         }

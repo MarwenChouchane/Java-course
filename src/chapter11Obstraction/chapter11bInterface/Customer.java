06df5ba6da2a2d1bf0ff54f31a08d5f30a035e31
MarwenChouchane <chouchane.marwen@gmail.com>
2022-01-04 11:49:12 +0000

    chapter11bInterface

---
+++ src/chapter11Obstraction/chapter11bInterface/Customer.java
@@ -0,0 +1,8 @@
+package chapter11Obstraction.chapter11bInterface;
+
+public class Customer {
+    public static void main(String[] arg){
+        Product book = new Book();
+        book.setPrice(10);
+    }
+}
---
+++ src/chapter11Obstraction/chapter11bInterface/Product.java
@@ -0,0 +1,16 @@
+package chapter11Obstraction.chapter11bInterface;
+
+public interface Product {
+    double getPrice();
+    void setPrice(double price);
+
+    String getName();
+    void setName(String name);
+
+    String getColor();
+    void setColor(String color);
+
+    default String getBarcode(){
+        return "no Barcode!";
+    }
+}
